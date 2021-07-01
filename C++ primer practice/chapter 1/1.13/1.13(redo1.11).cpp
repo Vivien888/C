ebc0d82796ff4d51c73b88723daa95821f71127f
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-01 13:17:54 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 1/1.13/1.13(redo1.11).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Please input two num:" << std::endl;
+	int start = 0, end = 0;
+	std::cin >> start >> end;
+	if (start <= end) {
+		for (; start <= end; ++start)
+{
+			std::cout << start << " ";
+		}
+	}
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
