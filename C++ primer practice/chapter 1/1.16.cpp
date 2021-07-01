53a28f8516b536d742811e4d89fadc37af265a2b
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-01 13:18:35 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 1/1.16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "请输入一些数，按Ctrl+z表示结束" << std::endl;
+	int sum = 0, val = 0;
+	for (; std::cin >> val;)
+		sum += val;
+    std::cout << "读入的数之和为" << sum << std::endl;
+	system("pause");
+	return 0;
+}
