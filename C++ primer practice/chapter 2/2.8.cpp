20a9052cd0fb8204cb32afb1695b87d2e0c12b51
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-03 09:34:44 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 2/2.8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "2\x4d\012";
+	std::cout << "2\tM\n";
+
+	/*
+	std::cout << 2 << "\115\012";
+	std::cout << 2 << "\t\115\012";
+	*/
+
+	system("pause");
+	return 0;
+}
