edcb008ca9cca857bae32665bc390600dca9d9a2
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-03 08:53:37 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 1/1.19(redo 1.11).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Please input two num:" << std::endl;
+	int start = 0, end = 0;
+	std::cin >> start >> end;
+	if (start <= end) {
+		while (start <= end)
+		{
+			std::cout << start << " ";
+			++start;
+		}
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << "start should be smaller than end !!!"
+			;
+	}
+	system("pause");
+	return 0;
+}
