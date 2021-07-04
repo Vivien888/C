054fb2c8396423cb044b1726cc71ef2920005a22
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-04 14:15:37 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 3/3.1/3.1(redo 1.10).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+// 使用过using声明使得cout和endl在程序中可见
+using namespace std;
+int main()
+{
+	int val = 10;
+	while (val >= 0)
+	{
+		cout << val << " ";
+		--val;
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
