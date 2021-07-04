c5118b1c3f9f0a2c2d0e2506e3cb819c7a962426
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-04 14:16:06 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 3/3.1/3.1(redo 1.11).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// 使用过using声明使得cout和endl在程序中可见
+using namespace std;
+int main()
+{
+	cout << "Please input two num:" << endl;
+	int start = 0, end = 0;
+	cin >> start >> end;
+	if (start <= end) {
+		while (start <= end)
+		{
+			cout << start << " ";
+			++start;
+		}
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
