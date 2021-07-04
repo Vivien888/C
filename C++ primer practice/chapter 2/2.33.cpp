97ec246daa36ef8953398b3caa5d28ad5c6a0ac1
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-04 10:11:55 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 2/2.33.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include<typeinfo>
+int main()
+{
+	auto a = 42;
+	cout << typeid(a).name() << endl;
+	const auto b = 42;
+	const int &bi = b;
+	cout << typeid(b).name() << endl;
+	auto c = bi;
+	cout << typeid(c).name() << endl;
+	auto d = &a;
+	cout << typeid(d).name() << endl;
+	auto e = &b;
+	cout << typeid(e).name() << endl;
+	auto &g = b;
+	cout << typeid(g).name() << endl;
+	system("pause");
+	return 0;
+}
