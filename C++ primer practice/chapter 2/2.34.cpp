6190faf7bdc66a3775e60aabaf2565887512b637
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-04 10:12:10 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 2/2.34.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include<typeinfo>
+int main()
+{
+	auto a = 42;
+	std::cout << a << endl;
+	const auto b = 42;
+	const int &bi = b;
+	std::cout << b << endl;
+	auto c = bi;
+	std::cout << c << endl;
+	auto d = &a;
+	std::cout << d << endl;
+	auto e = &b;
+	std::cout << e << endl;
+	auto &g = b;
+	std::cout << g << endl;
+	system("pause");
+	return 0;
+}
