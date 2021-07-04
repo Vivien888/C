1ad6b42b6a8b51fe24a2c08ff1beab49df150b3c
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-04 14:16:52 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 3/3.2/3.2(hang).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()		// 使用getline一次读入一整行
+{
+	string s;
+	// 循环读取，每次读入一整行，直至文件结束或遇到异常输入
+	cout << "请输入字符串，可以包含空格：" << endl;
+	while (getline(cin, s))
+		cout << s << endl;
+	system("pause");
+	return 0;
+}
