174526be47ddd832a219ea45b7e375c70f82b04c
Vivien888 <86181419+Vivien888@users.noreply.github.com>
2021-07-07 23:17:18 +0800

    Add files via upload

---
+++ C++ primer practice/chapter 3/3.7/3.7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << "请输入一个字符串，可以包含空格：" << endl;
+	getline(cin, s);		// 读取整行，遇回车符结束
+	for (char &c : s)		// 依次处理字符串中的每一个字符
+	{
+		c = 'X';
+	}
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
---
+++ C++ primer practice/chapter 3/3.8/3.8(redo 3.6 using for).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << "请输入一个字符串，可以包含空格：" << endl;
+	getline(cin, s);
+	for (int i = 0; s[i] != '\0';++i)
+	{
+		s[i] = 'X';
+	}
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
